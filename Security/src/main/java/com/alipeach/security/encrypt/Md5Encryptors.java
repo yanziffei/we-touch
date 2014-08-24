db34d704158d7283dc3ba237af2d5bd2640300eb
Christian Chen <christian.chan@qq.com>
2014-08-24 23:04:29 +0800

    updated Md5Encryptors

--- Security/src/main/java/com/alipeach/security/encrypt/Md5Encryptors.java
+++ Security/src/main/java/com/alipeach/security/encrypt/Md5Encryptors.java
@@ -9,9 +9,13 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Chen Haoming
@@ -24,7 +28,7 @@ public class Md5Encryptors implements IrreversibleEncryptors {
 
     private String charset = "iso-8859-1";
 
-    static String SALT = "fgshgsdHGhdg4%$4F$R$";
+    private static final String SALT = "fgshgsdHGhdg4%$4F$R$";
 
     public Md5Encryptors () {
         try {
@@ -76,4 +80,63 @@ public class Md5Encryptors implements IrreversibleEncryptors {
         }
     }
 
+    /**
+     * @author Chen Haoming
+     */
+    static class Md5SaltedHtpServletRequestWrapper extends HttpServletRequestWrapper {
+
+        private static final String J_PASSWORD = "j_password";
+
+        private HttpServletRequest request;
+
+        /**
+         * Constructs a request object wrapping the given request.
+         *
+         * @param request
+         *
+         * @throws IllegalArgumentException if the request is null
+         */
+        public Md5SaltedHtpServletRequestWrapper (HttpServletRequest request) {
+            super (request);
+            this.request = request;
+        }
+
+        @Override
+        public String getParameter (String name) {
+            String parameter = request.getParameter (name);
+            return (J_PASSWORD.equals (name) && null != parameter) ? parameter + SALT : parameter;
+        }
+
+        @Override
+        public Map<String, String[]> getParameterMap () {
+            Map<String, String[]> map = request.getParameterMap ();
+            if (map.containsKey (J_PASSWORD) && null != map.get (J_PASSWORD)) {
+                Map<String, String[]> newMap = new HashMap<> (map);
+                newMap.put (J_PASSWORD, copyStrings (map.get (J_PASSWORD)));
+                return Collections.unmodifiableMap (newMap);
+            }
+            return map;
+        }
+
+        @Override
+        public String[] getParameterValues (String name) {
+            String[] passwords = request.getParameterValues (name);
+
+            if (J_PASSWORD.equals (name)) {
+                if (null != passwords && passwords.length > 0) {
+                    return copyStrings (passwords);
+                }
+            }
+
+            return passwords;
+        }
+
+        private String[] copyStrings (String[] passwords) {
+            String[] newPasswords = new String[passwords.length];
+            for (int i = 0; i < passwords.length; ++ i) {
+                newPasswords[i] = (null == passwords[i]) ? null : passwords[i] + SALT;
+            }
+            return newPasswords;
+        }
+    }
 }
